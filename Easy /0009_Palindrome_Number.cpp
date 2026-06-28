1967250647767787ad2133e9caf294b53ed888ba
sujalshakyawal10 <sujalshakyawal10@gmail.com>
2026-06-28 17:04:11 +0530

    Update 0009_Palindrome_Number.cpp

--- Easy /0009_Palindrome_Number.cpp
+++ Easy /0009_Palindrome_Number.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+       if (x < 0) {
+            return false;
+        }
 
+        long long original = x;
+        long long reverse = 0;
+
+        while (x > 0) {
+            reverse = reverse * 10 + (x % 10);
+            x /= 10;
+        }
+
+        return original == reverse;
+    }
+};
