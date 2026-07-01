b0a71921899c9cd0cb2f92aff98c439c4b115b34
Sujal Shakyawal <sujalshakyawal10@gmail.com>
2026-07-01 06:03:18 +0530

    Update 0033_Search_in Rotated_Sorted_Array.cpp

--- Medium/0033_Search_in Rotated_Sorted_Array.cpp
+++ Medium/0033_Search_in Rotated_Sorted_Array.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        for(int i =0; i<nums.size();i++){
-            if(nums[i]==target){
-                return i;
+    int search(vector<int>& A, int target) {
+        int st=0,end=A.size()-1;
+
+        while(st<=end){
+            int mid = st + (end-st)/2;
+            if(A[mid]==target){
+            return mid;
+            }
+
+            if(A[st]<=A[mid]){//left
+               if(A[st]<=target && target<=A[mid]){
+                    end = mid -1;
+               }else{
+                    st = mid + 1;
+               }
+            }else{//right
+                if(A[mid]<=target && target<=A[end]){
+                    st = mid + 1;
+                }else{
+                    end = mid-1;
+                }
             }
         }
         return -1;
